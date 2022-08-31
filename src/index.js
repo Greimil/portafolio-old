bd882d06272597212765d6de547ff366af18b401
Greimil <greimilnunez@gmail.com>
2022-08-31 19:02:45 -0400

    hashrouter added and imported

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./componentes";
 import Aboutme from "./componentes/Aboutme";
 import Contact from "./componentes/Contact";
